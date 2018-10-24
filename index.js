45562c5b96a14aa2ab3809c497a3dbceddef230a
timeother <37954020+timeother@users.noreply.github.com>
2018-10-24 21:04:39 +1100

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var customHeaderRequest = request.defaults({
 
 setInterval(function() {
   if (min_position) {
-    customHeaderRequest.get(process.env.UPDATE_URL+min_position, function(err, resp, body){
+    customHeaderRequest.get(process.env.UPDATE_URL+encodeURIComponent(min_position), function(err, resp, body){
       try {
         var myObj = JSON.parse(body);
         if (typeof myObj.max_position !== typeof undefined && myObj.max_position !== false) {
